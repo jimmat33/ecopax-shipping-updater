9c4fbb43a4cf60b1e9eec7cbbd611cd0c5734322
Jimmy Pando <jwm6532@psu.edu>
2022-08-17 13:20:42 -0400

    Reset gitignore

--- run-updater-script.bat
+++ run-updater-script.bat
@@ -1,7 +1,10 @@
 git remote add origin 'https://github.com/jimmat33/ecopax-shipping-updater.git'
 git pull origin main
-pause
 cd ecopax-shipping-updater-code-files
+pause
 .\venv\Scripts\activate
+pause
 python ecopax_shipping_updater.py
+pause
 deactivate
+pause
