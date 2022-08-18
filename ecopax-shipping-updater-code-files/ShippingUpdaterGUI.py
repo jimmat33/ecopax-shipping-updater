2b9b59513f912f2e793e3510dfb11a047cc7677b
Jimmy Pando <jwm6532@psu.edu>
2022-08-18 10:06:17 -0400

    Added messagebox errors

--- ecopax-shipping-updater-code-files/ShippingUpdaterGUI.py
+++ ecopax-shipping-updater-code-files/ShippingUpdaterGUI.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import *
+from tkinter import messagebox
 import tkinter.font
 import os
 from tkinter.ttk import *
@@ -165,205 +166,212 @@ class ShippingUpdaterGUI(object):
 
 
     def import_spreadsheet_btn_click(self):
-        process_lst = []
-        
-        filename_list = filedialog.askopenfilenames(initialdir = "", title = "Select a File", filetypes = (("all files", "*.*"),))
-
-        
-        #just for now, will multiprocess this
-        for fp in filename_list:
-            excel_file = ExcelFile(fp)
-
-            self.xcel_file_list.append(excel_file)
-
-            file_name = fp.split('/')[-1]
-
-            self.excel_sheet_frame.insert(parent = '', index = 'end', iid = self.excel_index, text = '', values = (file_name,))
-            self.excel_index += 1
-
-
-        if len(self.xcel_file_list) == 1:
-                self.xcel_file_list[0].parse_workbook()
-
-        else:
-            for xcel_file in self.xcel_file_list:
-                xcel_process = multiprocessing.Process(target= xcel_file.parse_workbook)
-                process_lst.append(xcel_process)
-
-            for prse_process in process_lst:
-                prse_process.start()
-
-            for prse_process in process_lst:
-                prse_process.join()
-
-        container_lst = db_get_all_containers_gui()
-        sorted_list = sorted(container_lst, key = operator.itemgetter(1))
-
-        for cont in sorted_list:
-            self.cont_frame.insert(parent = '', index = 'end', iid = self.cont_table_index, text = '', values = (cont[0], cont[1], cont[2], cont[3],))
-            self.cont_table_index += 1
-
-        tf_items = self.get_all_treeview_items(self.cont_frame)
-        cont_num_list = []
-        [cont_num_list.append(val[0]) for val in tf_items]
-
-        cont_len = len(cont_num_list)
-        self.cont_num_label['text'] = f'Total Containers: {cont_len}'
-        
-
-    def remove_spreadsheet_btn_click(self):
-        
         try:
-            selected_item_index = self.excel_sheet_frame.focus()
-            item = self.excel_sheet_frame.item(selected_item_index)
-            if selected_item_index != '':
-
-                file_name = str(item['values'][0])
-                filepath_list = db_get_all_excel_filepaths()
-                filepath = ''
-
-                for data in filepath_list:
-                    if data[0].find(file_name) != -1:
-                        filepath = data[0]
-
-                db_remove_excel_file(filepath)
-                
-                self.excel_sheet_frame.delete(selected_item_index)
-
-            listOfEntriesInTreeView = self.cont_frame.get_children()
-
-
-            for trvw_item in listOfEntriesInTreeView:
-                if filepath in self.cont_frame.item(trvw_item)['values'][3].split('<'):
-                    self.cont_frame.delete(trvw_item)
-
-            tf_items = self.get_all_treeview_items(self.cont_frame)
-            cont_num_list = []
-            [cont_num_list.append(val[0]) for val in tf_items]
-
-            cont_len = len(cont_num_list)
-            self.cont_num_label['text'] = f'Total Containers: {cont_len}'
-
-
-        except Exception:
-            listOfEntriesInTreeView = self.cont_frame.get_children()
-
-            self.excel_sheet_frame.delete(selected_item_index)
-
-            for trvw_item in listOfEntriesInTreeView:
-                if self.cont_frame.item(trvw_item)['values']['fp'] == filepath:
-                    self.excel_sheet_frame.delete(trvw_item)
-
-            tf_items = self.get_all_treeview_items(self.cont_frame)
-            cont_num_list = []
-            [cont_num_list.append(val[0]) for val in tf_items]
-
-            cont_len = len(cont_num_list)
-            self.cont_num_label['text'] = f'Total Containers: {cont_len}'
-
-
-
-    def run_search_btn_click(self):
-        
-        print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Starting Run')
-        self.time_ran_label['text'] = ''
-        is_running = True
-        start_time = time.perf_counter()
-
-        excel_file_list = db_get_all_excel_files()
-        for xcel_sheet in excel_file_list:
-            try:
-                test_wb = load_workbook(filename=xcel_sheet[0])
-                test_wb.save(xcel_sheet[0])
-            except Exception as e:
-                print(e)
-                error_message = f'Please close file:\n\n {xcel_sheet[0]}'
-                tkinter.messagebox.showwarning(title='Open Excel File Error', message=error_message)
-                is_running = False
-                break
-        
-
-        if is_running:
+            process_lst = []
             
-            p1 = multiprocessing.Process(target=cosco_search)
-            p2 = multiprocessing.Process(target=evergreen_search)
-            p4 = multiprocessing.Process(target=maersk_search)
-            p5 = multiprocessing.Process(target=one_search)
+            filename_list = filedialog.askopenfilenames(initialdir = "", title = "Select a File", filetypes = (("all files", "*.*"),))
 
-            cma_list = get_divided_containers_by_carrier('CMA CGM')
-            cma_process_lst = []
-
-            for lst_chunk in cma_list:
-                p_cma = multiprocessing.Process(target=cma_search, args=(lst_chunk,))
-                cma_process_lst.append(p_cma)
-
-            p1.start()
-            print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Started Cosco')
-            p2.start()
-            print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Started Evergreen')
-            p4.start()
-            print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Started maersk')
-            p5.start()
-            print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Started one')
-    
-            for srch_process in cma_process_lst:
-                srch_process.start()
-                print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Started cma')
-    
-            p1.join()
-            print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Finished Cosco')
-            p2.join()
-            print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Finished Evergreen')
-            p4.join()
-            print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Finished maersk')
-            p5.join()
-            print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Finished one')
-   
-            for srch_process in cma_process_lst:
-                srch_process.join()
-                print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Finished cma')
-    
-            hmm_search()
-            hapag_search()
-
-
-            print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Finished search')
-            print(f'\n\nDone, Time Ran: {(time.perf_counter() - start_time)/60} minutes')
-
-            print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Started sheet modification')
-            modify_sheets()
-            print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Started sheet modification')
-
-            for item in self.cont_frame.get_children():
-                self.cont_frame.delete(item)
             
-            
-            self.cont_table_index = 1
+            #just for now, will multiprocess this
+            for fp in filename_list:
+                excel_file = ExcelFile(fp)
+
+                self.xcel_file_list.append(excel_file)
+
+                file_name = fp.split('/')[-1]
+
+                self.excel_sheet_frame.insert(parent = '', index = 'end', iid = self.excel_index, text = '', values = (file_name,))
+                self.excel_index += 1
+
+
+            if len(self.xcel_file_list) == 1:
+                    self.xcel_file_list[0].parse_workbook()
+
+            else:
+                for xcel_file in self.xcel_file_list:
+                    xcel_process = multiprocessing.Process(target= xcel_file.parse_workbook)
+                    process_lst.append(xcel_process)
+
+                for prse_process in process_lst:
+                    prse_process.start()
+
+                for prse_process in process_lst:
+                    prse_process.join()
+
             container_lst = db_get_all_containers_gui()
             sorted_list = sorted(container_lst, key = operator.itemgetter(1))
 
             for cont in sorted_list:
                 self.cont_frame.insert(parent = '', index = 'end', iid = self.cont_table_index, text = '', values = (cont[0], cont[1], cont[2], cont[3],))
                 self.cont_table_index += 1
+
+            tf_items = self.get_all_treeview_items(self.cont_frame)
+            cont_num_list = []
+            [cont_num_list.append(val[0]) for val in tf_items]
+
+            cont_len = len(cont_num_list)
+            self.cont_num_label['text'] = f'Total Containers: {cont_len}'
+        except Exception:
+            messagebox.showerror('Import Error', 'There was an error importing your file. Please try another file, or contact James Mattison')
+        
+
+    def remove_spreadsheet_btn_click(self):
+        try:
+            try:
+                selected_item_index = self.excel_sheet_frame.focus()
+                item = self.excel_sheet_frame.item(selected_item_index)
+                if selected_item_index != '':
+
+                    file_name = str(item['values'][0])
+                    filepath_list = db_get_all_excel_filepaths()
+                    filepath = ''
+
+                    for data in filepath_list:
+                        if data[0].find(file_name) != -1:
+                            filepath = data[0]
+
+                    db_remove_excel_file(filepath)
+                    
+                    self.excel_sheet_frame.delete(selected_item_index)
+
+                listOfEntriesInTreeView = self.cont_frame.get_children()
+
+
+                for trvw_item in listOfEntriesInTreeView:
+                    if filepath in self.cont_frame.item(trvw_item)['values'][3].split('<'):
+                        self.cont_frame.delete(trvw_item)
+
+                tf_items = self.get_all_treeview_items(self.cont_frame)
+                cont_num_list = []
+                [cont_num_list.append(val[0]) for val in tf_items]
+
+                cont_len = len(cont_num_list)
+                self.cont_num_label['text'] = f'Total Containers: {cont_len}'
+
+
+            except Exception:
+                listOfEntriesInTreeView = self.cont_frame.get_children()
+
+                self.excel_sheet_frame.delete(selected_item_index)
+
+                for trvw_item in listOfEntriesInTreeView:
+                    if self.cont_frame.item(trvw_item)['values']['fp'] == filepath:
+                        self.excel_sheet_frame.delete(trvw_item)
+
+                tf_items = self.get_all_treeview_items(self.cont_frame)
+                cont_num_list = []
+                [cont_num_list.append(val[0]) for val in tf_items]
+
+                cont_len = len(cont_num_list)
+                self.cont_num_label['text'] = f'Total Containers: {cont_len}'
+        except Exception:
+            messagebox.showerror('Removal Error', 'There was an error removing your file. Please try another file, or contact James Mattison')
+
+
+
+    def run_search_btn_click(self):
+        try:
+            print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Starting Run')
+            self.time_ran_label['text'] = ''
+            is_running = True
+            start_time = time.perf_counter()
+
+            excel_file_list = db_get_all_excel_files()
+            for xcel_sheet in excel_file_list:
+                try:
+                    test_wb = load_workbook(filename=xcel_sheet[0])
+                    test_wb.save(xcel_sheet[0])
+                except Exception as e:
+                    print(e)
+                    error_message = f'Please close file:\n\n {xcel_sheet[0]}'
+                    tkinter.messagebox.showwarning(title='Open Excel File Error', message=error_message)
+                    is_running = False
+                    break
             
 
-            db_set_all_cont_false()
+            if is_running:
+                
+                p1 = multiprocessing.Process(target=cosco_search)
+                p2 = multiprocessing.Process(target=evergreen_search)
+                p4 = multiprocessing.Process(target=maersk_search)
+                p5 = multiprocessing.Process(target=one_search)
 
-            error_lst = db_get_all_errors()
+                cma_list = get_divided_containers_by_carrier('CMA CGM')
+                cma_process_lst = []
 
-            for err in error_lst:
-                self.error_log_frame.insert(parent = '', index = 'end', iid = self.error_index, text = '', values = (err[0],))
-                self.error_index += 1
+                for lst_chunk in cma_list:
+                    p_cma = multiprocessing.Process(target=cma_search, args=(lst_chunk,))
+                    cma_process_lst.append(p_cma)
+
+                p1.start()
+                print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Started Cosco')
+                p2.start()
+                print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Started Evergreen')
+                p4.start()
+                print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Started maersk')
+                p5.start()
+                print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Started one')
+        
+                for srch_process in cma_process_lst:
+                    srch_process.start()
+                    print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Started cma')
+        
+                p1.join()
+                print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Finished Cosco')
+                p2.join()
+                print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Finished Evergreen')
+                p4.join()
+                print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Finished maersk')
+                p5.join()
+                print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Finished one')
+    
+                for srch_process in cma_process_lst:
+                    srch_process.join()
+                    print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Finished cma')
+        
+                hmm_search()
+                hapag_search()
 
 
-           
-            time_ran = round(((time.perf_counter() - start_time)/60), 2)
+                print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Finished search')
+                print(f'\n\nDone, Time Ran: {(time.perf_counter() - start_time)/60} minutes')
 
-            self.time_ran_label['text'] = f'Time Ran: {time_ran} Minutes'
+                print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Started sheet modification')
+                modify_sheets()
+                print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Started sheet modification')
 
-        else:
+                for item in self.cont_frame.get_children():
+                    self.cont_frame.delete(item)
+                
+                
+                self.cont_table_index = 1
+                container_lst = db_get_all_containers_gui()
+                sorted_list = sorted(container_lst, key = operator.itemgetter(1))
 
-            print('Run search btn clicked')
+                for cont in sorted_list:
+                    self.cont_frame.insert(parent = '', index = 'end', iid = self.cont_table_index, text = '', values = (cont[0], cont[1], cont[2], cont[3],))
+                    self.cont_table_index += 1
+                
+
+                db_set_all_cont_false()
+
+                error_lst = db_get_all_errors()
+
+                for err in error_lst:
+                    self.error_log_frame.insert(parent = '', index = 'end', iid = self.error_index, text = '', values = (err[0],))
+                    self.error_index += 1
+
+
+            
+                time_ran = round(((time.perf_counter() - start_time)/60), 2)
+
+                self.time_ran_label['text'] = f'Time Ran: {time_ran} Minutes'
+
+            else:
+
+                print('Run search btn clicked')
+        except Exception:
+            messagebox.showerror('Search Error', 'There was an completing the container search. Please try another file, or contact James Mattison')
 
 
     def get_all_treeview_items(self, tv_frame):
